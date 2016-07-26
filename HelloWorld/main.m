5b2478bcacd025cd01809a1e0854e6eaa4c3e035
sunnyqin <sunny@interactivelabs.co>
2016-07-26 22:52:52 +0800

    Start Project

---
+++ HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by SunyQin on 7/26/16.
+//  Copyright © 2016 ilabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+use_frameworks!
+target “HelloWorld” do
+pod 'Crashlytics', '~> 3.6'
+pod 'Fabric', '~> 1.6'
+end
+
