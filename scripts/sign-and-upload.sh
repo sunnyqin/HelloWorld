b60f72ddedbf8b8fd6a5e44b6cada4e9b1b72db2
sunnyqin <sunny@interactivelabs.co>
2016-07-27 22:11:59 +0800

    create staging

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -4,8 +4,8 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   exit 0
 fi
 
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-  echo "Testing on a branch other than master/staging. No deployment will be done."
+if [[ "$TRAVIS_BRANCH" != "staging" ]]; then
+  echo "Testing on a branch other than staging. No deployment will be done."
   exit 0
 fi
 
@@ -25,4 +25,4 @@ echo "**************************************************"
 echo "*   (Master Branch) Uploading To Crashlytics     *"
 echo "**************************************************"
 
-Pods/Crashlytics/submit 0c31dbdd91674d7e38c2970702a8ed1296c2b427 60c8c37aabf092fb185bb414226b3c778eb2f32c630ce3584780de36c08014e5 -ipaPath "$OUTPUTDIR/$APP_NAME.ipa"
+Pods/Crashlytics/submit d1735a5902bb0852d662b1917c4f691cfc391c5c 2fa52a54d0c2cbf8b079374579123798cba998c9e2a441ba7e95b79c542d99ae -ipaPath "$OUTPUTDIR/$APP_NAME.ipa"
