62fa6c8798de05edb7870f38dd5acd03cddddc8d
sunnyqin <sunny@interactivelabs.co>
2016-08-01 15:06:23 +0800

    fix version

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -16,4 +16,6 @@ echo "********************"
 # Travis fetches a shallow clone. We use commit count until HEAD for build number. In order to assure that the count is correct we have to `unshallow` Travis' clone.
 git fetch --unshallow
 
+gem update fastlane
+
 fastlane beta
